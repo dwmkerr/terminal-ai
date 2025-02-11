48b75fd1b6f0b92683d16b687d5059900e34de14
Dave Kerr <dwmkerr@gmail.com>
2025-02-11 10:48:18 +0000

    feat(docs): terminal recording + docs (#23)

---
+++ scripts/demo-to-svg.sh
@@ -0,0 +1,2 @@
+svg-term --in ./docs/casts/new-recording.cast --out docs/casts/new-recording.svg --window --no-cursor --from=100
+
--- scripts/record-demo.sh
+++ scripts/record-demo.sh
@@ -1,13 +1,14 @@
 # For this script to work:
 # 1. make sure you have an iterm profile named 'dwmkerr-recording'
-# 2. This profile should run the command: `/usr/local/bin/bash --noprofile --norc -c 'export TMUX="ignore"; bash'`
+# 2. This profile is in my dotfiles
 # 3. The profile should be windowed, and sized to 80x15.
 osascript <<EOF
 tell application "iTerm"
     set newWindow to (create window with profile "dwmkerr-recording")
     tell current session of newWindow
         write text "clear"
-        write text "AWS_PROFILE=dwmkerr; /usr/local/bin/asciinema rec --overwrite --title "Boxes Demo" ~/repos/github/dwmkerr/boxes/docs/620124.cast"
+        write text "cd ~/repos/github/dwmkerr/terminal-ai"
+        write text "asciinema rec --overwrite --title 'Terminal AI' ~/repos/github/dwmkerr/terminal-ai/docs/casts/new-recording.cast"
     end tell
 end tell
 EOF
