5d7622ff0ce7e5000e86d2a86e9cc70ae6b44471
Dave Kerr <dwmkerr@gmail.com>
2025-02-11 16:17:31 +0000

    docs: cleanup readme / add casts
    Also improves the cast recording scripts and process.

--- scripts/record-demo.sh
+++ scripts/record-demo.sh
@@ -1,3 +1,10 @@
+# - Install [asciinema](https://asciinema.org/) `brew install asciinema`
+# - Check that you have your profiles setup as documented in `./scripts/record-demo.sh`
+# - Run the script to start a 'clean' terminal `./scripts/record-demo.sh`
+# - Download your recording, e.g. to `./docs/620124.cast`
+# - Install [svg-term-cli](https://github.com/marionebl/svg-term-cli) `npm install -g svg-term-cli`
+# - Convert to SVG: `./scripts/demo-to-svg.sh`
+#
 # For this script to work:
 # 1. make sure you have an iterm profile named 'dwmkerr-recording'
 # 2. This profile is in my dotfiles
